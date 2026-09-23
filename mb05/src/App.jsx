24bca1ce293d62df666f98de008060e35bfab61c
MichalAdamskiAM <adamski.michal.am@gmail.com>
2026-09-23 09:26:24 +0200

    MB05: Navbar

--- mb05/src/App.jsx
+++ mb05/src/App.jsx
@@ -1,8 +1,13 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 return (
-    <div className="container mt-4">
-        <h1>Galeria zdjęć</h1>
-    </div>
+    <>
+        <Navbar />
+        <div className="container mt-4">
+            <h1>Galeria zdjęć</h1>
+        </div>
+    </>
 )}
 
 export default App
